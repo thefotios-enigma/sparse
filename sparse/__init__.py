449c75d21d3158630bc5be79c690a60cfc002578
Nils Werner <nils.werner@gmail.com>
2017-12-30 10:11:18 +0100

    Initial implementation of a simple 2-operand einsum

--- sparse/__init__.py
+++ sparse/__init__.py
@@ -1,3 +1,3 @@
-from .core import COO, tensordot, concatenate, stack, dot, triu, tril, random
+from .core import COO, tensordot, concatenate, stack, dot, triu, tril, random, einsum
 
 __version__ = '0.1.1'
--- sparse/core.py
+++ sparse/core.py
@@ -4,6 +4,7 @@ from collections import Iterable, defaultdict, deque
 from functools import reduce
 import numbers
 import operator
+import copy
 
 import numpy as np
 import scipy.sparse
@@ -350,6 +351,42 @@ class COO(object):
             self.has_duplicates)
 
     __repr__ = __str__
+
+    def diag_axis(self, axis=-1):
+        """ Diagonalize axis.
+
+        Similar to using :code:`np.diag(a)`, however for higher dimensional
+        arrays.
+
+        Doing this on a dense array would be prohibitevly expensive as
+        a lot of zeros would be introduced. However for a sparse array it
+        simply means replicating the appropriate row in the :code:`coords`
+        array.
+
+        Parameters
+        ----------
+        axis : integer
+            The axis to be diagonalized.
+
+        Returns
+        -------
+        COO
+            The output of the computation. A view of all unchanged
+            attributes is returned.
+
+        See also
+        --------
+        np.diag : NumPy equivalent for diagonalizing 1D arrays
+        """
+        out = copy.copy(self)
+        # normalize axis index
+        naxis = list(range(out.ndim))[axis]
+        shape = list(out.shape)
+        shape.insert(naxis, shape[naxis])
+
+        out.coords = np.insert(out.coords, naxis, out.coords[naxis, :], 0)
+        out.shape = tuple(shape)
+        return out
 
     @staticmethod
     def _reduce(method, *args, **kwargs):
@@ -1649,3 +1686,113 @@ def random(
         ar.sum_duplicates()
 
     return ar
+
+
+def tensordot2(A, B, sum=None, multiply=None):
+    """ Tensordot that supports elementwise multiplication of axes
+    without a subsequent sum-contraction.
+
+    A sum contraction "can be prevented" if the corresponding axis is
+    diagonalized. This principle can be seen in the most simple case by
+    comparing
+
+    ..code::
+
+        a = np.arange(5)
+        b = np.arange(5)
+
+        a @ b           # sum-contracted to scalar
+        np.diag(a) @ b  # vector with elementwise products
+
+    Diagonalizing axes on a dense array would of course be prohibitevly
+    costly but it is really cheap for sparse matrices.
+
+    Parameters
+    ----------
+    A, B : COO
+        The input arrays.
+    sum : list[list[int]]
+        The axes to multiply and sum-contract over.
+    multiply : list[list[int]]
+        The axes to multiply over.
+
+    Returns
+    -------
+    COO
+        The output of the computation.
+
+    See Also
+    --------
+    einsum : Einstein summation function using this
+    COO.diag_axis : Diagonalize axes of sparse array
+    """
+    if sum is None:
+        sum = [[], []]
+    else:
+        sum = list(sum)
+
+    if multiply is None:
+        multiply = [[], []]
+    else:
+        multiply = list(multiply)
+
+    # For each multiply[0] we are adding one axis, thus we need to increment
+    # all following items by one: (0, 1, 2) -> (0, 2, 4)
+    # We need to account that the array may be unsorted
+    idx = np.argsort(multiply[0])
+    post_multiply = multiply[0]
+    for i, v in enumerate(idx):
+        post_multiply[v] += i
+
+    for i in post_multiply:
+        A = A.diag_axis(i)
+
+    sum[0] += post_multiply
+    sum[1] += multiply[1]
+
+    return tensordot(A, B, axes=sum)
+
+
+def einsum(ops, *args):
+    """ Evaluates the Einstein summation convention on the operands.
+
+    Parameters
+    ----------
+    ops : string
+        Specifies the subscripts for summation.
+    *args : COO
+        These are the arrays for the operation. Only exactly two are supported.
+
+    Returns
+    -------
+    COO
+        The output of the computation.
+
+    See Also
+    --------
+    numpy.einsum : NumPy equivalent function
+    tensordot2 : Non-contracting tensordot implementation
+    """
+
+    if len(args) != 2:
+        raise ValueError("Currently only two operands are supported")
+
+    inops, outops = ops.split('->')
+    inops = inops.split(',')
+
+    # All indices that are in input AND in output are multiplies
+    multiplies = list(set(inops[0]) & set(inops[1]) & set(outops))
+    # All indices that are in input BUT NOT in output are sum contractions
+    sums = list((set(inops[0]) & set(inops[1])) - set(outops))
+
+    # Map sums and indices to axis integers
+    multiplies = [[inop.find(x) for x in multiplies] for inop in inops]
+    sums = [[inop.find(x) for x in sums] for inop in inops]
+
+    # Generate character->axis lookup table to get final transpose
+    lookup = {x: i for i, x in enumerate(inops[0])}
+    lookup2 = {x: i + len(lookup) for i, x in enumerate(inops[1]) if x not in lookup}
+    lookup = {**lookup, **lookup2}
+    transpose = [lookup[x] for x in outops]
+
+    return tensordot2(*args, sum=sums, multiply=multiplies).transpose(transpose)
